f90981454307222da10a906ece5b13b14ba59c05
Naukyz <zhao_yukuan@163.com>
2017-07-05 12:08:45 +0800

    Commit

--- src/main/java/ThinkInJava/c21/c2/ThreadVariations.java
+++ src/main/java/ThinkInJava/c21/c2/ThreadVariations.java
@@ -5,7 +5,7 @@ public class ThreadVariations {
         new InnerRunnable1("InnerRunnable1");
         new InnerRunnable2("InnerRunnable2");
         new InnerThread1("InnerThread1");
-        new InnerThread1("InnerThread1");
+        new InnerThread2("InnerThread2");
         new ThreadMethod("ThreadMethod").runTask();
     }
 }
