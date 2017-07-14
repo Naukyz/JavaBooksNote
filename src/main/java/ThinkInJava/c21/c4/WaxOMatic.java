938e5ae64afa99024a172844f723950c848607e9
Naukyz <zhao_yukuan@163.com>
2017-07-14 18:38:53 +0800

    Commit

---
+++ src/main/java/ThinkInJava/c21/c4/WaxOMatic.java
@@ -0,0 +1,91 @@
+package ThinkInJava.c21.c4;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by zhao_yukuan@163.com
+ * on 2017/7/14
+ */
+public class WaxOMatic {
+    public static void main(String[] args) throws Exception {
+        Car car = new Car();
+        ExecutorService executor = Executors.newCachedThreadPool();
+        executor.execute(new WaxOff(car));
+        executor.execute(new WaxOn(car));
+        Thread.sleep(5000);
+        executor.shutdownNow();
+    }
+}
+
+class Car {
+    private boolean waxOn = false;
+
+    public synchronized void waxed() {
+        waxOn = true;
+        notifyAll();
+    }
+
+    public synchronized void buffed() {
+        waxOn = false;
+        notifyAll();
+    }
+
+    public synchronized void waitForWaxing() throws InterruptedException {
+        while (!waxOn) {
+            wait();
+        }
+    }
+
+    public synchronized void waitForBuffing() throws InterruptedException {
+        while (waxOn) {
+            wait();
+        }
+    }
+}
+
+class WaxOn implements Runnable {
+    private Car car;
+
+    public WaxOn(Car car) {
+        this.car = car;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                System.out.println("Wax On ");
+                Thread.sleep(200);
+                car.waxed();
+                car.waitForBuffing();
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        System.out.println("Ending Wax On Task");
+    }
+}
+
+class WaxOff implements Runnable {
+    private Car car;
+
+    public WaxOff(Car car) {
+        this.car = car;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                car.waitForWaxing();
+                System.out.println("Wax Off");
+                Thread.sleep(200);
+                car.buffed();
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        System.out.println("Ending Wax Off Task");
+    }
+}
