b307d3723d02429dc1fdfea3e7dfba6289c61887
Naukyz <zhao_yukuan@163.com>
2017-07-15 07:55:01 +0800

    Commit

---
+++ src/main/java/ThinkInJava/c21/c4/NotifyVsNotifyAll.java
@@ -0,0 +1,85 @@
+package ThinkInJava.c21.c4;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by zhao_yukuan@163.com
+ * on 2017/7/15
+ */
+public class NotifyVsNotifyAll {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            executor.execute(new Task());
+        }
+        executor.execute(new Task2());
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            boolean prod = true;
+
+            @Override
+            public void run() {
+                if (prod) {
+                    System.out.println("\nnotify()");
+                    Task.blocker.prod();
+                    prod = false;
+                } else {
+                    System.out.println("\nnotifyAll()");
+                    Task.blocker.prodAll();
+                    prod = true;
+                }
+            }
+        }, 400, 400);
+        Thread.sleep(5000);
+        timer.cancel();
+        System.out.println("\nTimer canceled");
+        Thread.sleep(500);
+        System.out.println("Task2.blocker.prodAll()");
+        Task2.blocker.prodAll();
+        Thread.sleep(500);
+        System.out.println("\nShutting down");
+        executor.shutdownNow();
+    }
+}
+
+class Blocker {
+    synchronized void waitingCall() {
+        try {
+            while (!Thread.interrupted()) {
+                wait();
+                System.out.println(Thread.currentThread() + " ");
+            }
+        } catch (InterruptedException e) {
+             System.out.println(e.toString());
+        }
+    }
+
+    synchronized void prod() {
+        notify();
+    }
+
+    synchronized void prodAll() {
+        notifyAll();
+    }
+}
+
+class Task implements Runnable {
+    static Blocker blocker = new Blocker();
+
+    @Override
+    public void run() {
+        blocker.waitingCall();
+    }
+}
+
+class Task2 implements Runnable {
+    static Blocker blocker = new Blocker();
+
+    @Override
+    public void run() {
+        blocker.waitingCall();
+    }
+}
---
+++ src/main/java/ThinkInJava/c21/c4/SomeCondition.java
@@ -0,0 +1,43 @@
+package ThinkInJava.c21.c4;
+
+/**
+ * Created by zhao_yukuan@163.com
+ * on 2017/7/15
+ */
+public class SomeCondition {
+    static SharedMonitor sharedMonitor = new SharedMonitor();
+    static boolean someCondition = false;
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        // T1
+        synchronized (sharedMonitor) {
+            // <setup someCondition for T2>
+            sharedMonitor.notify();
+        }
+
+        // T2
+        while (someCondition) {
+            // Point 1
+            synchronized (sharedMonitor) {
+                sharedMonitor.wait();
+            }
+        }
+
+        // T2 判断是 true 进入 point1 同时执行 T1 的 notify 然后 T2 wait T2 会永远错过notify 死锁
+
+        // T2 改进
+        synchronized (sharedMonitor) {
+            while (someCondition) {
+                sharedMonitor.wait();
+            }
+        }
+    }
+
+
+}
+
+class SharedMonitor {
+
+}
