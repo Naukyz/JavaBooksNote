7e01735be47a1708783f475c649257c8a9b37144
Naukyz <zhao_yukuan@163.com>
2017-07-27 14:44:58 +0800

    Commit

---
+++ src/main/java/ThinkInJava/A.java
@@ -0,0 +1,16 @@
+package ThinkInJava;
+
+/**
+ * Created by zhao_yukuan@163.com
+ * on 2017/7/27
+ */
+public class A {
+}
+
+class B implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
