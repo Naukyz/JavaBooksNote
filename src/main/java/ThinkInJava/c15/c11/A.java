05d470179d9e751ed6a638dad3e32e126ea0a40c
Naukyz <zhao_yukuan@163.com>
2017-08-30 17:38:41 +0800

    Commit

--- src/main/java/ThinkInJava/c15/c11/A.java
+++ src/main/java/ThinkInJava/c15/c11/A.java
@@ -1,10 +1,10 @@
 package ThinkInJava.c15.c11;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -13,29 +13,34 @@ import java.util.List;
  */
 public class A {
     public static void main(String[] args) {
+//
+//        Person<Person> p1 = new Person<>("p1");
+//        Person<Person> p2 = new Person<>("p2");
+//        Person<Person> p3 = new Person<>("p3");
+//        Person<Person> p4 = new Person<>("p4");
+//
+//
+//        List<Person> list1 = new ArrayList<>(Arrays.asList(p1, p2));
+//        p3.setList(list1);
+//        List<Person> list2 = new ArrayList<>(Arrays.asList(p1, p2, p3));
+//        p4.setList(list2);
+//        List<Person> list3 = new ArrayList<>(Arrays.asList(p3, p4));
+//
+//        String s = b(list3);
+//
+//        List<Person<Person>> list4 = a(s, new TypeToken<List<Person<Person>>>() {
+//        });
+//        System.out.println(list4);
+//
+//        Person<Person> pp = list4.get(1);
+//        List<Person> list5 = pp.getList();
+//
+//        System.out.println(list5);
 
-        Person<Person> p1 = new Person<>("p1");
-        Person<Person> p2 = new Person<>("p2");
-        Person<Person> p3 = new Person<>("p3");
-        Person<Person> p4 = new Person<>("p4");
+
+        System.out.println(b("<script />"));
 
 
-        List<Person> list1 = new ArrayList<>(Arrays.asList(p1, p2));
-        p3.setList(list1);
-        List<Person> list2 = new ArrayList<>(Arrays.asList(p1, p2, p3));
-        p4.setList(list2);
-        List<Person> list3 = new ArrayList<>(Arrays.asList(p3, p4));
-
-        String s = b(list3);
-
-        List<Person<Person>> list4 = a(s, new TypeToken<List<Person<Person>>>() {
-        });
-        System.out.println(list4);
-
-        Person<Person> pp = list4.get(1);
-        List<Person> list5 = pp.getList();
-
-        System.out.println(list5);
 
 
     }
@@ -48,6 +53,17 @@ public class A {
     public static String b(Object s) {
         Gson gson = new Gson();
         return gson.toJson(s);
+    }
+
+
+    public static <T> T a2(String s, TypeReference<T> t) {
+        return JSON.parseObject(s, t.getType());
+    }
+
+    public static String b2(Object s) {
+        return JSON.toJSONString(s);
+
+
     }
 
 }
